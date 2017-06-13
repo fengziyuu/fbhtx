413d5eddea045fc38fed9f427649cfa277d99f66
2839259756@qq.com <2839259756@qq.com@6f36e62b-1b1e-4321-af43-fd634028e6ae>
2017-06-13 07:43:16 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/index.php

--- 1/index.php
+++ 1/index.php
@@ -1,1 +1,152 @@
-<!DOCTYPE html><html><head><meta charset="utf-8"><title>欢迎使用新浪云</title></head><body><h4>欢迎使用新浪云</h4><p>技术领先的新浪云正为您提供稳定的云计算服务。</p><p>使用中遇到问题欢迎提<a href="http://www.sinacloud.com/ucenter/workorderadd.html"target="_blank">工单</a>咨询，您会在第一时间得到专业的回答。</p><p>您可能需要了解以下内容：</p><ul><li><a href="http://www.sinacloud.com/doc/sae/tutorial/index.html?from=app_index"target="_blank">入门指南</a></li><li><a href="http://www.sinacloud.com/index/apply_ospace.html?from=app_index"target="_blank">免费申请成为合作伙伴</a></li><li><a href="http://www.sinacloud.com/index/applyqy.html?from=app_index"target="_blank">免费申请迁移指导</a></li><li><a href="http://www.sinacloud.com/index/faq/sort_id/1.html?from=app_index"target="_blank">常见问题</a></li><li><a href="http://weibo.com/saet"target="_blank">新浪云官方微博</a></li></ul><p>新浪云产品动态：</p><span id="news">加载中...</span><p><a href="http://www.sinacloud.com/news.html?from=app_index"target="_blank">更多>></a></p></body><script type="text/javascript"src="http://cdn.static.sinacloud.com/public/sinacloud_news.js"></script><p><b>提示：</b>此页面是默认首页 index.html、index.php 如果您不需要可以删除或修改。</p></html>
+<?php
+//token的验证操作 确认请求的确来自于微信服务器 而不是恶意的第三方
+define('TOKEN','fengziyu');
+function signature(){
+	//接收微信服务器的参数
+	$signature=$_GET['signature'];//微信加密签名  token  timestamp  nonce
+	$timestamp=$_GET['timestamp'];//时间戳
+	$nonce=$_GET['nonce'];//随机数
+	//把token   timestamp  nonce 存储在数组里
+	$stmArr=array($nonce,$timestamp,TOKEN);
+	//排序 sort SORT_STRING 快速的排序
+	sort($stmArr,SORT_STRING);
+	//转换为字符串
+	$stmStr=implode($stmArr);
+	//对字符串做加密(sha1)
+	$stmStr=sha1($stmStr);
+	//把加密以后的字符串和微信加密签名 signature 做对比 标识该请求的确来自于微信服务器
+	if($stmStr==$signature){
+		return true;
+	}else{
+		return false;
+	}
+
+}
+if(signature()){
+	//原样返回echostr 随机字符串 接口接入成功  成为开发者
+	$echostr=$_GET['echostr'];
+	if($echostr){
+		echo $echostr;
+		exit;
+	}
+}
+
+
+//微信公众平台接收普通用户发送的消息（XML） (post)  1    1
+$postData=$HTTP_RAW_POST_DATA;
+//判断
+if(!$postData){
+	echo "error";
+	exit();
+}
+// <xml>
+//  <ToUserName><![CDATA[toUser]]></ToUserName> 接收方 微信公众账号服务器 
+//  <FromUserName><![CDATA[fromUser]]></FromUserName> 发送方 普通微信用户
+//  <CreateTime>1348831860</CreateTime> 发送时间
+//  <MsgType><![CDATA[text]]></MsgType> 发送消息类型
+//  <Content><![CDATA[]]>1</Content>  发送的内容
+//  <MsgId>1234567890123456</MsgId>
+//  </xml>
+
+
+
+//把接收到的xml数据包 转换为对象
+//SimpleXMLElement  是新对象的类 
+//LIBXML_NOCDATA 设置文本节点  CDATA标签里 xml文本不进行解析
+$obj=simplexml_load_string($postData,"SimpleXMLElement",LIBXML_NOCDATA);
+
+
+// 微信公众账号服务器 被动回复消息
+
+
+// <xml>
+//  <ToUserName><![CDATA[toUser]]></ToUserName>   普通微信用户
+//  <FromUserName><![CDATA[fromUser]]></FromUserName>  微信公众账号服务器
+//  <CreateTime>1348831860</CreateTime>
+//  <MsgType><![CDATA[text]]></MsgType>
+//  <Content><![CDATA[]]>1</Content>
+//  <MsgId>1234567890123456</MsgId>
+//  </xml>
+
+//获取发送方 (微信用户 )-》当做被动回复的接收方
+$FromUserName=$obj->FromUserName;
+//获取接收方 (微信公众账号服务器 )-》当做被动回复的发送方
+$ToUserName=$obj->ToUserName;
+
+//获取用户发送的消息类型
+$MsgType=$obj->MsgType;
+switch($MsgType){
+	case 'text':
+	//获取用户发送过来的文本消息(当做回复消息)
+	$replyMsg=$obj->Content;
+
+	//把回复的信息封装在xml数据包里
+	$replyXml="<xml>
+					<ToUserName><![CDATA[%s]]></ToUserName>
+					<FromUserName><![CDATA[%s]]></FromUserName>
+					<CreateTime>%s</CreateTime>
+					<MsgType><![CDATA[text]]></MsgType>
+					<Content><![CDATA[%s]]></Content>
+				</xml>";
+
+	break;
+
+	//接收图片消息被动回复图片消息
+	case 'image':
+	//获取用户发送过来的图片消息(当做回复消息)
+	$replyMsg=$obj->MediaId;
+	//把回复的消息封装在xml数据包里
+	$replyXml="<xml>
+					<ToUserName><![CDATA[%s]]></ToUserName>
+					<FromUserName><![CDATA[%s]]></FromUserName>
+					<CreateTime>%s</CreateTime>
+					<MsgType><![CDATA[image]]></MsgType>
+					<Image>
+					<MediaId><![CDATA[%s]]></MediaId>
+					</Image>
+				</xml>";
+
+	break;
+
+	//接收语音消息和被动回复语音
+	case "voice":
+	//获取用户发送的语音(当做回复消息)
+	$replyMsg=$obj->MediaId;
+	//封装回复的xml
+	$replyXml="<xml>
+					<ToUserName><![CDATA[%s]]></ToUserName>
+					<FromUserName><![CDATA[%s]]></FromUserName>
+					<CreateTime>%s</CreateTime>
+					<MsgType><![CDATA[voice]]></MsgType>
+					<Voice>
+					<MediaId><![CDATA[%s]]></MediaId>
+					</Voice>
+				</xml>";
+
+	break;
+
+	//事件推送 关注 
+	case 'event':
+	$Event=$obj->Event;
+	switch($Event){
+		//关注
+		case "subscribe":
+		$replyMsg="欢迎来到大林子平台,回复1:逛淘宝,回复2上京东";
+		//封装xml数据包
+		$replyXml="<xml>
+					<ToUserName><![CDATA[%s]]></ToUserName>
+					<FromUserName><![CDATA[%s]]></FromUserName>
+					<CreateTime>%s</CreateTime>
+					<MsgType><![CDATA[text]]></MsgType>
+					<Content><![CDATA[%s]]></Content>
+				</xml>";
+
+		break;
+	}
+	break;
+}
+
+//把%符号替换为一个作为参数传递变量
+$result=sprintf($replyXml,$FromUserName,$ToUserName,time(),$replyMsg);
+echo $result;
+ ?>
