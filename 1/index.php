6c0c7c526f30a64ea51177816f0eaf17f87830ca
2839259756@qq.com <2839259756@qq.com@6f36e62b-1b1e-4321-af43-fd634028e6ae>
2017-06-13 08:37:15 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/index.php

--- 1/index.php
+++ 1/index.php
@@ -101,7 +101,7 @@ switch($MsgType){
 					<FromUserName><![CDATA[%s]]></FromUserName>
 					<CreateTime>%s</CreateTime>
 					<MsgType><![CDATA[image]]></MsgType>
-					<PicUrl><![CDATA[./124.gif]]></PicUrl>
+					<PicUrl><![CDATA[./1.jpg]]></PicUrl>
                     <MediaId><![CDATA[%s]]></MediaId>
 				</xml>";
 
