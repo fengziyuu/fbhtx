117a6d49462b2e2b362017ad7ac4c040988c2e06
2839259756@qq.com <2839259756@qq.com@6f36e62b-1b1e-4321-af43-fd634028e6ae>
2017-06-13 08:47:02 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/a.php

---
+++ 1/a.php
@@ -0,0 +1,27 @@
+<?php
+//$str 微信公众平台服务器接收到的xml数据包 
+// $str="<xml>
+            <ToUserName><![CDATA[toUser]]></ToUserName>
+            <FromUserName><![CDATA[fromUser]]></FromUserName>
+            <CreateTime>1351776360</CreateTime>
+            <MsgType><![CDATA[location]]></MsgType>
+            <Location_X>23.134521</Location_X>
+            <Location_Y>113.358803</Location_Y>
+            <Scale>20</Scale>
+            <Label><![CDATA[位置信息]]></Label>
+            <MsgId>1234567890123456</MsgId>
+         </xml>";
+
+
+// 	//把xml数据包转换为对象
+$obj=simplexml_load_string($str,"SimpleXMLElement",LIBXML_NOCDATA);
+
+var_dump($obj);
+// 	echo $obj->ToUserName;
+
+$format="there are %d monkeys in the %s";
+$num=3;
+$l='tree';
+//把%符号替换为一个作为参数传递变量
+echo sprintf($format,$num,$l);
+ ?>
