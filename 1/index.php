c252595012376e079f63e4bb9e5ab2f0c255abae
2839259756 <2839259756@qq.com>
2017-06-14 11:56:19 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/index.php

--- 1/index.php
+++ 1/index.php
@@ -1,111 +1,145 @@
-<?php
-//用sdk封装消息交互接口
-define('TOKEN','fengziyu');
-if(!isset($_GET['echostr']))
+<?php 
+//用sdk 封装 消息交互接口 (过程版)
+//定义token
+define("TOKEN",'fengziyu');
+if(!isset($_GET['echostr'])){
+	//调用响应消息函数
 	responseMsg();
-else
+}else{
+	//实现网址的接入 验证消息的函数
 	valid();
+}
 
 //验证消息
-function valid()
-{
-	if(checksignature())
-	{
-		$echostr = $_GET['echostr'];
+function valid(){
+	if(checksignature()){
+		//获取随机字符串
+		$echostr=$_GET['echostr'];
+		//返回随机字符串 网址接入成功
 		echo $echostr;
 		exit;
-	}	
-	else
-	{
-		echo 'error';
+	}else{
+		echo "error";
 		exit;
 	}
 }
 
-function checksignature()
-{
-	$signature = $_GET['signature'];
-	$timestamp = $_GET['timestamp'];
-	$nonce = $_GET['nonce'];
-	$stampArr = array($nonce,$timestamp,TOKEN);
-	sort($stampArr,SORT_STRING);
-	$stampStr = implode($stampArr);
-	$stampStr = sha1($stampStr);
-	if($stampStr == $signature)
+function checksignature(){
+	//获取加密签名
+	$signature=$_GET['signature'];
+	//时间戳
+	$timestamp=$_GET['timestamp'];//时间戳
+	$nonce=$_GET['nonce'];//随机数
+	//把token   timestamp nonce 存储在数组里 排序
+	$stempArr=array($nonce,$timestamp,TOKEN);
+	sort($stempArr,SORT_STRING);
+	//转换为字符串
+	$stempStr=implode($stempArr);
+	//加密字符串
+	$stempStr=sha1($stempStr);
+	if($stempStr==$signature){
 		return true;
-	else
+	}else{
 		return false;
+	}
 }
 
-function responseMsg()
-{
-	$postData = $GLOBALS[HTTP_RAW_POST_DATA];
-	if(!$postData)
-	{
-		echo 'error';
+//响应消息函数
+function responseMsg(){
+	//1.获取用户发送的 xml消息
+	// $GLOBALS 全局操作
+	$postData=$GLOBALS[HTTP_RAW_POST_DATA];
+	if(!$postData){
+		echo "error";
 		exit;
 	}
-	$obj = simplexml_load_string($postData,'SimpleXMLElement',LIMXML_NOCDATA);
-	$FromUserName = $obj->FromUserName;
-	$ToUserName = $obj->ToUserName;
-	$MsgType = $obj->MsgType;
-	switch($MsgType)
-	{
+
+	//2.解析xml
+	$object=simplexml_load_string($postData,"SimpleXMLElement",LIBXML_NOCDATA);
+	//获取用户发送的消息类型
+	$MsgType=$object->MsgType;
+
+	switch($MsgType){
 		case 'text':
-			echo receiveText($obj);
+		//接收消息
+		echo receiveText($object);
 		break;
 
 		case 'image':
-			echo receiveImage($obj);
+		echo receiveImage($object);
 		break;
-
+		//地理位置
 		case 'location':
-			echo receiveLocation($obj);
+		echo receiveLocation($object);
 		break;
-
+		//链接
 		case 'link':
-			echo receiveLink($obj);
+		echo receiveLink($object);
 		break;
-
 		default:
 		break;
 	}
 }
 
-function receiveText($obj)
-{
-	$content = $obj->Content;
+//接收文本消息函数
+function receiveText($obj){
+	//获取用户发送的文本消息
+	$content=$obj->Content;
 	return replyText($obj,$content);
 }
 
-function receiveImage($obj)
-{
-	$imageArr = array(
-		'PicUrl'=>$obj->PicUrl,
-		'MediaId'=>$obj->MediaId
+//接收图片消息
+function receiveImage($obj){
+	//图片消息id和图片链接存储在数组里
+	$imageArr=array(
+		"PicUrl"=>$obj->PicUrl,
+		"MediaId"=>$obj->MediaId
 		);
+
 	return replyImage($obj,$imageArr);
 }
 
-function receiveLocation($obj)
-{
-	$locationArr = array(
-		'Location_Y'=>at6[[]],
-		'Location_X'=>$obj->Location_X,
-		'Label'=>$obj->Label
+//接收地理位置消息
+function receiveLocation($obj){
+	//经度 纬度 地理位置信息 存储在数组里
+	$locationArr=array(
+		"Location_Y"=>"at6[[]]
+		"Location_X"=>$obj->Location_X,
+		"Label"=>$obj->Label
+
 		);
+
 	return replyText($obj,$locationArr['Location_Y']);
+
 }
 
-function receiveLink($obj)
-{
-	$linkArr = array('Url'=>$obj->Url);
+//接收链接消息
+function receiveLink($obj){
+	//把Url存储在数组里
+	$linkArr=array(
+		'Url'=>$obj->Url
+		);
+
 	return replyText($obj,$linkArr['Url']);
 }
+//回复文本消息
+function replyText($obj,$content){
+	//封装回复的xml数据包
+	$replyXml="<xml>
+					<ToUserName><![CDATA[%s]]></ToUserName>
+					<FromUserName><![CDATA[%s]]></FromUserName>
+					<CreateTime>%s</CreateTime>
+					<MsgType><![CDATA[text]]></MsgType>
+					<Content><![CDATA[%s]]></Content>
+				</xml>";
+	$resultstr=sprintf($replyXml,$obj->FromUserName,$obj->ToUserName,time(),$content);
+	return $resultstr;
+}
 
-function replyImage($obj,$imageArr)
-{
-	$replyXml = "<xml>
+//回复图片消息
+function replyImage($obj,$imageArr){
+	//封装xml
+	$replyXml="<xml>
 					<ToUserName><![CDATA[%s]]></ToUserName>
 					<FromUserName><![CDATA[%s]]></FromUserName>
 					<CreateTime>%s</CreateTime>
@@ -114,19 +148,7 @@ function replyImage($obj,$imageArr)
 					<MediaId><![CDATA[%s]]></MediaId>
 					</Image>
 				</xml>";
-	$resultstr = sprintf($replyXml,$FromUserName,$ToUserName,time(),$imageArr['MediaId']);
-	echo $resultstr;
-}
-
-function replyText($obj,$content)
-{
-	$replyXml="<xml>
-					<ToUserName><![CDATA[%s]]></ToUserName>
-					<FromUserName><![CDATA[%s]]></FromUserName>
-					<CreateTime>%s</CreateTime>
-					<MsgType><![CDATA[text]]></MsgType>
-					<Content><![CDATA[%s]]></Content>
-				</xml>";
-	$resultstr=sprintf($replyXml,$FromUserName,$ToUserName,time(),$content);
+	$resultstr=sprintf($replyXml,$obj->FromUserName,$obj->ToUserName,time(),$imageArr['MediaId']);
 	return $resultstr;
 }
+ ?>
