5ad407628785b54187bca9b765faffc70680ee10
2839259756 <2839259756@qq.com>
2017-06-14 14:13:51 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/index.php

--- 1/index.php
+++ 1/index.php
@@ -113,7 +113,7 @@ function receiveEvent($obj)
 	$event = $obj->Event;
 	switch($event){
 		case 'subscribe':
-		global $replyMsg = "欢迎来到我的平台,回复1:可以逛淘宝,回复2:可以上京东";
+		$replyMsg = "欢迎来到我的平台,回复1:可以逛淘宝,回复2:可以上京东";
 		replyText($obj,$replyMsg);
 		break;
 	}
