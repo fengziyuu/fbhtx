3715aaee4d5e417666c954107b34dfbe7b366d39
2839259756@qq.com <2839259756@qq.com@6f36e62b-1b1e-4321-af43-fd634028e6ae>
2017-06-14 12:23:17 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/index.php

--- 1/index.php
+++ 1/index.php
@@ -111,7 +111,7 @@ function receiveLink($obj)
 function receiveEvent($obj)
 {
 	$event = $obj->Event;
-	switch($Event){
+	switch($event){
 		case 'subscribe':
 		$replyMsg = "欢迎来到我的平台,回复1:可以逛淘宝,回复2:可以上京东";
 		replyText($obj,$replyMsg);
