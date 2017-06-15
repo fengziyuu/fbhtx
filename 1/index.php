f56d2ecff806eb9ee611bf9a271914ffb31e4c30
2839259756@qq.com <2839259756@qq.com@6f36e62b-1b1e-4321-af43-fd634028e6ae>
2017-06-15 08:27:08 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/index.php

--- 1/index.php
+++ 1/index.php
@@ -260,5 +260,5 @@ function curls()
     //json_decode()
     $s=json_decode($output,true);
     // var_dump($s);
-    return htmlspecialchars($s['content']);
+    return htmlspecialchars_decode(str_replace("<br >","",$s['content']));
 }
