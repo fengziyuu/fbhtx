8c39cc10d722fc59931ec836dfa375863b706524
2839259756@qq.com <2839259756@qq.com@6f36e62b-1b1e-4321-af43-fd634028e6ae>
2017-06-15 12:34:54 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/index.php

--- 1/index.php
+++ 1/index.php
@@ -63,5 +63,282 @@
 	//4.关闭
 	curl_close($cn);
 	echo $output;
+	<?php 
 
+//微信公众平台基础接口PHP SDK(面向对象)
+
+define("TOKEN",'fengziyu');
+
+$wechat = new Wechat_base_api();
+
+if(!isset($_GET['echostr']))
+	$wechat->responseMsg();//调用响应消息函数
+else
+	$wechat->valid();//实现网址接入.调用验证消息函数
+
+class Wechat_base_api
+{
+	//验证消息
+	public function valid()
+	{
+		if($this->checkSignature())
+		{
+			$echostr = $_GET['echostr'];
+			echo $echostr;
+			exit;
+		}
+		else
+		{
+			echo 'error';
+			exit;
+		}
+	}
+
+	//检查签名
+	public function checkSignature()
+	{
+		//获取微信服务器GET请求的4个参数
+		$signature = $_GET['signature'];
+		$timestamp = $_GET['timestamp'];
+		$nonce = $_GET['nonce'];
+
+		//定义一个数组 存储其中三个参数  分别是timestamp nonce token
+		$tempArr = array($nonce,$timestamp,TOKEN);
+
+		//进行排序
+		sort($tempArr,SORT_STRING);
+
+		//将数组转换成字符串
+		$tmpStr = implode($tempArr);
+
+		//进行sha1加密算法
+		$tmpStr = sha1($tmpStr);
+
+		//判断请求是否来自微信服务器,对比$tmpStr 和 $signature
+		if($tmpStr == $signature)
+			return true;
+		else
+			return false;
+	}
+
+	//响应消息
+	public function responseMsg()
+	{
+		//根据用户传过来的消息类型进行不同的响应
+
+		//1.接受微信服务器POST过来的数据 XML数据包
+		$postData = $GLOBALS[HTTP_RAW_POST_DATA];
+		if(!$postData)
+		{
+			echo 'error';
+			exit;
+		}
+
+		//2.解析XML数据包
+		$obj = simplexml_load_string($postData,'SimpleXMLElement',LIBXML_NOCDATA);
+
+		//3.获取消息类型
+		$MsgType = $obj->MsgType;
+		switch($Msgtype)
+		{
+			case 'text':
+	 				//接收文本消息
+	 				echo $this->receiveText($obj);
+	 			break;
+
+	 		case 'image':
+ 		        	//接收图片消息
+ 		        	echo $this->receiveImage($obj);	
+	 			break;
+
+	 		case 'voice':
+ 		        	//接收语音消息
+ 		        	echo $this->receiveVoice($obj);	
+	 			break;
+	 			
+	 		
+	 		case  'link':
+	 				//接收链接消息
+	 				echo $this->receiveLink($obj);
+	 				break;
+
+	 		case 'location':
+ 					//接收地理位置消息
+ 					$data=$this->receiveLocation($obj);
+ 					$ch=curl_init();
+					$url="http://api.map.baidu.com/telematics/v3/weather?location=".$data['Location_Y'].",".$data['Location_X']."&output=json&ak=6b219a615eb77699a10eb54054959a2e";
+ 					//2.设置变量
+					curl_setopt($ch,CURLOPT_URL,$url);
+					//把数据以数据流的形式显示出
+					curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+					$output=curl_exec($ch);
+					curl_close($ch);
+					$s=json_decode($output,true);
+					$data=$s['results'][0]['weather_data'];
+					$dataarr=array();
+					//遍历
+					foreach($data as $key=>$value){
+						$dataarr[]=array(
+							'Title'=>$value['date'],
+							'Description'=>$value['weather'].$value['wind'].$value['temperature'],
+							'PicUrl'=>$value['dayPictureUrl'],
+							'Url'=>''
+							);
+					}
+
+					//回复图文消息
+					echo $this->replyNews($object,$dataarr);		
+	 				break; 
+
+	 		default:
+	 			break;
+		}
+
+	}
+
+	//接收地理位置信息
+	private function receiveLocation($obj){
+		//把获取到的地理位置信息存储在数组
+		$locationArr=array(
+			"Location_X"=>$obj->Location_X,
+			"Location_Y"=>$obj->Location_Y,
+			"Label"=>$obj->Label
+			);
+
+		return $locationArr;
+	}
+
+	//接受文本消息
+	private function receiveText($obj)
+	{
+		//获取文本消息的内容
+		$content = $obj->Content;
+		//发送文本消息
+		return $this->replyText($obj,$content);
+	}
+
+	//接受图片消息
+	private function receiveImage($obj)
+	{
+		//获取图片消息的内容
+		$imageArr = array(
+				'PicUrl'=>$obj->PicUrl,
+				'MediaId'=>$obj->MediaId,
+			);
+		//发送图片消息
+		return $this->replyImage($obj,$imageArr);
+	}
+
+	//接受语音消息
+	private function receiveVoive($obj)
+	{
+		//获取语音消息内容
+		$voiceArr = array(
+				'MediaId'=>$obj->MediaId,
+				'Format'=>$obj->Format,
+			);
+		//发送语音消息
+		return $this->replyVoice($obj,$voiceArr);
+	}
+
+	//接收链接消息
+	private function receiveLink($obj)
+	{
+		//接收链接消息的内容
+		$linkArr = array(
+				"Title"=>$obj->Title,
+				"Description"=>$obj->Description,
+				"Url"=>$obj->Url
+			);
+		//回复文本消息
+		return $this->replyText($obj,"你发过来的链接地址是{$linkArr['Url']}");
+	}
+
+	//发送文本消息
+	private function replyText($obj,$content){
+		$replyXml = "<xml>
+					<ToUserName><![CDATA[%s]]></ToUserName>
+					<FromUserName><![CDATA[%s]]></FromUserName>
+					<CreateTime>%s</CreateTime>
+					<MsgType><![CDATA[text]]></MsgType>
+					<Content><![CDATA[%s]]></Content>
+					</xml>";
+	        //返回一个进行xml数据包
+
+		$resultStr = sprintf($replyXml,$obj->FromUserName,$obj->ToUserName,time(),$content);
+	        return $resultStr;		
+	}
+
+	//发送图片消息
+	private function replyImage($obj,$imageArr){
+		$replyXml = "<xml>
+					<ToUserName><![CDATA[%s]]></ToUserName>
+					<FromUserName><![CDATA[%s]]></FromUserName>
+					<CreateTime>%s</CreateTime>
+					<MsgType><![CDATA[image]]></MsgType>
+					<Image>
+					<MediaId><![CDATA[%s]]></MediaId>
+					</Image>
+					</xml>";
+	        //返回一个进行xml数据包
+
+		$resultStr = sprintf($replyXml,$obj->FromUserName,$obj->ToUserName,time(),$imageArr['MediaId']);
+	        return $resultStr;			
+	}
+
+	//回复语音消息
+	private function replyVoice($obj,$voiceArr)
+	{
+		$replyXml = "<xml>
+					<ToUserName><![CDATA[%s]]></ToUserName>
+					<FromUserName><![CDATA[%s]]></FromUserName>
+					<CreateTime>%s</CreateTime>
+					<MsgType><![CDATA[voice]]></MsgType>
+					<Voice>
+					<MediaId><![CDATA[%s]]></MediaId>
+					</Voice>
+					</xml>";
+	        //返回一个进行xml数据包
+
+		$resultStr = sprintf($replyXml,$obj->FromUserName,$obj->ToUserName,time(),$voiceArr['MediaId']);
+	        return $resultStr;		
+	}
+
+	//回复图文消息
+	private function replyNews($obj,$newsArr){
+		$itemStr = "";
+		if(is_array($newsArr))
+		{
+			foreach($newsArr as $item)
+			{
+				$itemXml ="<item>
+					<Title><![CDATA[%s]]></Title> 
+					<Description><![CDATA[%s]]></Description>
+					<PicUrl><![CDATA[%s]]></PicUrl>
+					<Url><![CDATA[%s]]></Url>
+					</item>";
+				$itemStr .= sprintf($itemXml,$item['Title'],$item['Description'],$item['PicUrl'],$item['Url']);
+			}
+
+		}
+
+		$replyXml = "<xml>
+					<ToUserName><![CDATA[%s]]></ToUserName>
+					<FromUserName><![CDATA[%s]]></FromUserName>
+					<CreateTime>%s</CreateTime>
+					<MsgType><![CDATA[news]]></MsgType>
+					<ArticleCount>%s</ArticleCount>
+					<Articles>
+						{$itemStr}
+					</Articles>
+					</xml> ";
+	        //返回一个进行xml数据包
+
+		$resultStr = sprintf($replyXml,$obj->FromUserName,$obj->ToUserName,time(),count($newsArr));
+	        return $resultStr;			
+	}	
+
+		
+}
+ ?>
  ?>
