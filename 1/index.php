ea939888aed8c9042079c0f946a8b47e1dbe89cb
2839259756@qq.com <2839259756@qq.com@6f36e62b-1b1e-4321-af43-fd634028e6ae>
2017-06-15 12:50:14 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/index.php

--- 1/index.php
+++ 1/index.php
@@ -1,338 +1,246 @@
 <?php
-//在开发模式下自定义菜单
-// (1) 获取access_token值
-	$appid="wx1ec677af86f59d7d"; 
-	//定义凭证秘钥
-	$appsecret="4c9a7d6f3f0413aa77a98d9ccbc08018";
-	//接口地址 
-	$url="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={$appid}&secret={$appsecret}";
-	$ch=curl_init();
-	curl_setopt($ch,CURLOPT_URL,$url);
-	curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-	$output=curl_exec($ch);
-	curl_close($ch);
-	// echo $output;
-	$access_token=json_decode($output,true);
-	// var_dump($access_token);
-	$access_token=$access_token['access_token'];
-	// (2)请求接口
 	
-	 
-	//设置请求的地址
-	// $url="http://localhost/c/opt.php";
-	$url="https://api.weixin.qq.com/cgi-bin/menu/create?access_token={$access_token}";
-	//携带的参数
-	
-	$post='{
-     "button":[
-     {	
-          "type":"click",
-          "name":"今日歌曲",
-          "key":"V1001_TODAY_MUSIC"
-      },
-      {
-           "name":"菜单",
-           "sub_button":[
-           {	
-               "type":"view",
-               "name":"搜索",
-               "url":"http://www.soso.com/"
-            },
-            {
-               "type":"view",
-               "name":"视频",
-               "url":"http://v.qq.com/"
-            },
-            {
-               "type":"click",
-               "name":"赞一下我们",
-               "key":"V1001_GOOD"
-            }]
-       }]
- 	}';
-	//1.初始化
-	$cn=curl_init();
-	//2.设置变量
-	curl_setopt($cn,CURLOPT_URL,$url);
-	//模拟post请求
-	curl_setopt($cn,CURLOPT_POST,1);
-	//post 发送内容
-	curl_setopt($cn,CURLOPT_POSTFIELDS,$post);
-	//3.执行
-	$output=curl_exec($cn);
-	//4.关闭
-	curl_close($cn);
-	echo $output;
 
-//用sdk封装消息交互接口
-define('TOKEN','fengziyu');
-if(!isset($_GET['echostr']))
-	responseMsg();
-else
-	valid();
+	//微信公众平台基础接口PHP SDK （面向对象版）
 
-//验证消息
-function valid()
-{
-	if(checksignature())
+	define("TOKEN","fengziyu");
+
+	$wechat = new Wechat_base_api();
+
+
+	if(!isset($_GET['echostr']))
 	{
-		$echostr = $_GET['echostr'];
-		echo $echostr;
-		exit;
-	}	
+		//调用响应消息函数
+		$wechat->responseMsg();
+	}
 	else
 	{
-		echo 'error';
-		exit;
+		//实现网址接入，调用验证消息函数	
+		$wechat->valid();
 	}
-}
 
-function checksignature()
-{
-	$signature = $_GET['signature'];
-	$timestamp = $_GET['timestamp'];
-	$nonce = $_GET['nonce'];
-	$stampArr = array($nonce,$timestamp,TOKEN);
-	sort($stampArr,SORT_STRING);
-	$stampStr = implode($stampArr);
-	$stampStr = sha1($stampStr);
-	if($stampStr == $signature)
-		return true;
-	else
-		return false;
-}
-
-function responseMsg()
-{
-	$postData = $GLOBALS[HTTP_RAW_POST_DATA];
-	if(!$postData)
-	{
-		echo 'error';
-		exit;
-	}
-	$obj = simplexml_load_string($postData,'SimpleXMLElement',LIBXML_NOCDATA);
-	$FromUserName = $obj->FromUserName;
-	$ToUserName = $obj->ToUserName;
-	$MsgType = $obj->MsgType;
-	switch($MsgType)
-	{
-		case 'text':
-			echo receiveText($obj);
-		break;
-
-		case 'image':
-			echo receiveImage($obj);
-		break;
-
-		case 'location':
-			echo receiveLocation($obj);
-		break;
-
-		case 'link':
-			echo receiveLink($obj);
-		break;
-
-		case 'event':
-			echo receiveEvent($obj);
-		break;
-
-		default:
-		break;
-	}
-}
-
-function receiveText($obj)
-{
-	$content = $obj->Content;
-	return replyText($obj,$content);
-}
-
-function receiveImage($obj)
-{
-	$imageArr = array(
-		'PicUrl'=>$obj->PicUrl,
-		'MediaId'=>$obj->MediaId
-		);
-	return replyImage($obj,$imageArr);
-}
-
-function receiveLocation($obj)
-{
-	$locationArr = array(
-		'Location_Y'=>"您所在的位置的经度".$obj->Location_Y,
-		'Location_X'=>"纬度".$obj->Location_X,
-		'Label'=>$obj->Label
-		);
-    $str = implode($locationArr);
-	return replyText($obj,$str);
-}
-
-function receiveLink($obj)
-{
-	$linkArr = array('Url'=>$obj->Url);
-	return replyText($obj,$linkArr['Url']);
-}
-
-function receiveEvent($obj)
-{
-	$event = $obj->Event;
-	switch($event){
-		case 'subscribe':
-		$replyMsg = "欢迎来到我的平台,回复1:可以逛淘宝,回复2:可以上京东;回复‘笑话’可以看笑话！";
-		//replyText($obj,$replyMsg);
-      	$replyXml="<xml>
-					<ToUserName><![CDATA[%s]]></ToUserName>
-					<FromUserName><![CDATA[%s]]></FromUserName>
-					<CreateTime>%s</CreateTime>
-					<MsgType><![CDATA[text]]></MsgType>
-					<Content><![CDATA[%s]]></Content>
-				</xml>";
-        $resultstr=sprintf($replyXml,$obj->FromUserName,$obj->ToUserName,time(),$replyMsg);
-		echo $resultstr;
-		break;
-	}
-}
-
-function replyImage($obj,$imageArr)
-{
-	$replyXml = "<xml>
-					<ToUserName><![CDATA[%s]]></ToUserName>
-					<FromUserName><![CDATA[%s]]></FromUserName>
-					<CreateTime>%s</CreateTime>
-					<MsgType><![CDATA[image]]></MsgType>
-					<Image>
-					<MediaId><![CDATA[%s]]></MediaId>
-					</Image>
-				</xml>";
-	$resultstr = sprintf($replyXml,$obj->FromUserName,$obj->ToUserName,time(),$imageArr['MediaId']);
-	echo $resultstr;
-}
-
-function replyText($obj,$content)
-{
-	if($content==1)
-		{
-			$dataArray = array(
-				array(
-					'Title'=>'浏览淘宝1111',
-					'Description'=>'this is a test',
-					'PicUrl'=>'https://img10.360buyimg.com/da/jfs/t5749/237/1364970330/39708/347c201a/59256b8fNbb8043f6.gif.jpg',
-					'Url'=>'https://www.taobao.com',
-					),
-				array(
-					'Title'=>'浏览淘宝2222',
-					'Description'=>'this is a test',
-					'PicUrl'=>'https://img11.360buyimg.com/mobilecms/s110x110_jfs/t2581/20/346515013/295406/cc1ec305/570f39c1Nebd3b947.jpg',
-					'Url'=>'https://www.jd.com',
-					),
-				);
-
-			$strwx = "";
-			foreach($dataArray as $k=>$v)
+	class Wechat_base_api{
+		//验证消息
+		public function valid(){
+			if($this->checkSignature())
 			{
-				$strwx .="<item>
-								<Title><![CDATA[".$v['Title']."]]></Title> 
-								<Description><![CDATA[".$v['Description']."]]></Description>
-								<PicUrl><![CDATA[".$v['PicUrl']."]]></PicUrl>
-								<Url><![CDATA[".$v['Url']."]]></Url>
-							</item>"; 
+				$echostr = $_GET["echostr"];
+				echo $echostr;
+				exit;
+			}
+			else
+			{
+				echo "error";
+				exit;
+			}
+		}
+
+		//检查签名
+		private function checkSignature()
+		{
+			//获取微信服务器GET请求的4个参数
+			$signature = $_GET['signature'];
+			$timestamp = $_GET['timestamp'];
+			$nonce = $_GET['nonce'];
+
+			//定义一个数组，存储其中3个参数，分别是timestamp，nonce和token
+			$tempArr = array($nonce,$timestamp,TOKEN);
+
+			//进行排序
+			sort($tempArr,SORT_STRING);
+
+			//将数组转换成字符串
+
+			$tmpStr = implode($tempArr);
+
+			//进行sha1加密算法
+			$tmpStr = sha1($tmpStr);
+
+			//判断请求是否来自微信服务器，对比$tmpStr和$signature
+			if($tmpStr == $signature)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}	
+
+		//响应消息
+		public function responseMsg(){
+			//根据用户传过来的消息类型进行不同的响应
+			//1、接收微信服务器POST过来的数据，XML数据包
+
+			$postData = $GLOBALS[HTTP_RAW_POST_DATA];
+
+			if(!$postData)
+			{
+				echo  "error";
+				exit();
 			}
 
+			//2、解析XML数据包
 
-			$replyXml = "<xml>
-							<ToUserName><![CDATA[%s]]></ToUserName>
-							<FromUserName><![CDATA[%s]]></FromUserName>
-							<CreateTime>%s</CreateTime>
-							<MsgType><![CDATA[news]]></MsgType>
-							<ArticleCount>".count($dataArray)."</ArticleCount>
-							<Articles>
-							".$strwx."
-							</Articles>
-						</xml>";
+		 	$object = simplexml_load_string($postData,"SimpleXMLElement",LIBXML_NOCDATA);
+
+		 	//获取消息类型
+		 	$MsgType = $object->MsgType;
+		 	switch ($MsgType) {
+		 		
+		 		case 'text':
+		 				//接收文本消息
+		 				 echo $this->receiveText($object);
+		 			break;
+		 		case 'image':
+		 		        	//接收图片消息
+		 		        	echo $this->receiveImage($object);	
+		 			break;
+		 			
+		 		
+		 		case  'link':
+		 				//接收链接消息
+		 				echo $this->receiveLink($object);
+		 				break;
+		 		default:
+		 			break;
+		 	}
 		}
-		elseif($content==2)
-		{
-			$dataArray = array(
-				array(
-					'Title'=>'浏览京东1111',
-					'Description'=>'this is a test',
-					'PicUrl'=>'https://img.alicdn.com/tfscom/i3/50983440/TB2ys6TrH8kpuFjy0FcXXaUhpXa_!!50983440.jpg_240x240.jpg',
-					'Url'=>'https://www.taobao.com',
-					),
-				array(
-					'Title'=>'浏览京东2222',
-					'Description'=>'this is a test',
-					'PicUrl'=>'https://img11.360buyimg.com/mobilecms/s110x110_jfs/t2581/20/346515013/295406/cc1ec305/570f39c1Nebd3b947.jpg',
-					'Url'=>'https://www.jd.com',
-					),
-				);
 
-			$strwx = "";
-			foreach($dataArray as $k=>$v)
+		
+		//接收文本消息
+		private function receiveText($obj){
+			//获取文本消息的内容
+			$content = $obj->Content;
+			//发送文本消息
+			return $this->replyText($obj,$content);
+		}
+
+		//接收图片消息
+		private function receiveImage($obj)
+		{
+			//获取图片消息的内容
+			$imageArr = array(
+				"PicUrl"=>$obj->PicUrl,
+				"MediaId"=>$obj->MediaId
+				);
+			//发送图片消息
+			return $this->replyImage($obj,$imageArr);
+		}
+
+		
+
+		//接收语言消息
+		private function receiveVoice($obj){
+			//获取语言消息内容
+			$voiceArr = array(
+					"MediaId"=>$obj->MediaId,
+					"Format"=>$obj->Format
+				);
+			//回复语言消息
+			return $this->replyVoice($obj,$voiceArr);
+		}
+
+		
+
+		//接收链接消息
+		private function receiveLink($obj)
+		{
+			//接收链接消息的内容
+			$linkArr = array(
+					"Title"=>$obj->Title,
+					"Description"=>$obj->Description,
+					"Url"=>$obj->Url
+				);
+			//回复文本消息
+			return $this->replyText($obj,"你发过来的链接地址是{$linkArr['Url']}");
+		}
+
+		//发送文本消息
+		private function replyText($obj,$content){
+			$replyXml = "<xml>
+						<ToUserName><![CDATA[%s]]></ToUserName>
+						<FromUserName><![CDATA[%s]]></FromUserName>
+						<CreateTime>%s</CreateTime>
+						<MsgType><![CDATA[text]]></MsgType>
+						<Content><![CDATA[%s]]></Content>
+						</xml>";
+		        //返回一个进行xml数据包
+
+			$resultStr = sprintf($replyXml,$obj->FromUserName,$obj->ToUserName,time(),$content);
+		        return $resultStr;		
+		}
+
+		//发送图片消息
+		private function replyImage($obj,$imageArr){
+			$replyXml = "<xml>
+						<ToUserName><![CDATA[%s]]></ToUserName>
+						<FromUserName><![CDATA[%s]]></FromUserName>
+						<CreateTime>%s</CreateTime>
+						<MsgType><![CDATA[image]]></MsgType>
+						<Image>
+						<MediaId><![CDATA[%s]]></MediaId>
+						</Image>
+						</xml>";
+		        //返回一个进行xml数据包
+
+			$resultStr = sprintf($replyXml,$obj->FromUserName,$obj->ToUserName,time(),$imageArr['MediaId']);
+		        return $resultStr;			
+		}
+
+		//回复语音消息
+		private function replyVoice($obj,$voiceArr)
+		{
+			$replyXml = "<xml>
+						<ToUserName><![CDATA[%s]]></ToUserName>
+						<FromUserName><![CDATA[%s]]></FromUserName>
+						<CreateTime>%s</CreateTime>
+						<MsgType><![CDATA[voice]]></MsgType>
+						<Voice>
+						<MediaId><![CDATA[%s]]></MediaId>
+						</Voice>
+						</xml>";
+		        //返回一个进行xml数据包
+
+			$resultStr = sprintf($replyXml,$obj->FromUserName,$obj->ToUserName,time(),$voiceArr['MediaId']);
+		        return $resultStr;		
+		}
+
+		
+
+		
+
+		//回复图文消息
+		private function replyNews($obj,$newsArr){
+			$itemStr = "";
+			if(is_array($newsArr))
 			{
-				$strwx .="<item>
-								<Title><![CDATA[".$v['Title']."]]></Title> 
-								<Description><![CDATA[".$v['Description']."]]></Description>
-								<PicUrl><![CDATA[".$v['PicUrl']."]]></PicUrl>
-								<Url><![CDATA[".$v['Url']."]]></Url>
-							</item>"; 
+				foreach($newsArr as $item)
+				{
+					$itemXml ="<item>
+						<Title><![CDATA[%s]]></Title> 
+						<Description><![CDATA[%s]]></Description>
+						<PicUrl><![CDATA[%s]]></PicUrl>
+						<Url><![CDATA[%s]]></Url>
+						</item>";
+					$itemStr .= sprintf($itemXml,$item['Title'],$item['Description'],$item['PicUrl'],$item['Url']);
+				}
+
 			}
 
-
 			$replyXml = "<xml>
-							<ToUserName><![CDATA[%s]]></ToUserName>
-							<FromUserName><![CDATA[%s]]></FromUserName>
-							<CreateTime>%s</CreateTime>
-							<MsgType><![CDATA[news]]></MsgType>
-							<ArticleCount>".count($dataArray)."</ArticleCount>
-							<Articles>
-							".$strwx."
-							</Articles>
-						</xml>";
-		}elseif($content=='笑话'){
-            $replyXml="<xml>
-					<ToUserName><![CDATA[%s]]></ToUserName>
-					<FromUserName><![CDATA[%s]]></FromUserName>
-					<CreateTime>%s</CreateTime>
-					<MsgType><![CDATA[text]]></MsgType>
-					<Content><![CDATA[%s]]></Content>
-				</xml>";
-				$content = curls();
-            
-        }else{
-			$replyXml="<xml>
-					<ToUserName><![CDATA[%s]]></ToUserName>
-					<FromUserName><![CDATA[%s]]></FromUserName>
-					<CreateTime>%s</CreateTime>
-					<MsgType><![CDATA[text]]></MsgType>
-					<Content><![CDATA[%s]]></Content>
-				</xml>";
+						<ToUserName><![CDATA[%s]]></ToUserName>
+						<FromUserName><![CDATA[%s]]></FromUserName>
+						<CreateTime>%s</CreateTime>
+						<MsgType><![CDATA[news]]></MsgType>
+						<ArticleCount>%s</ArticleCount>
+						<Articles>
+							{$itemStr}
+						</Articles>
+						</xml> ";
+		        //返回一个进行xml数据包
+
+			$resultStr = sprintf($replyXml,$obj->FromUserName,$obj->ToUserName,time(),count($newsArr));
+		        return $resultStr;			
 		}
-	$resultstr=sprintf($replyXml,$obj->FromUserName,$obj->ToUserName,time(),$content);
-	echo $resultstr;
-}
-
-
-function curls()
-{
-	header("Content-type:text/html;charset=utf-8");
-    //curl 模拟get请求1 截取部分数据 以数据流的形式显示出 而不是整个页面
-    //1.初始化
-    $ch=curl_init();
-    //笑话接口
-    $url="http://www.kuitao8.com/api/joke";
-    //2.设置变量
-    curl_setopt($ch,CURLOPT_URL,$url);
-    //把数据以数据流的形式显示出
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-    //3.执行
-    $output=curl_exec($ch);
-    //4.关闭curl
-    curl_close($ch);
-
-    // echo $output;
-    //json_decode()
-    $s=json_decode($output,true);
-    return htmlspecialchars_decode(str_replace("<br />","",$s['content']));
-}
- ?>
+	}
